98fccb4f9b01e210b74bec1bef866878e48166e6
ShaunKolich <employmeinc@gmail.com>
2019-06-03 10:36:05 -0400

    Setting up app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
+
+const App = () => {
+  return (
+    <div>
+      <CalculatorDisplay/>
+    </div>
+  );
+};
+
+export default App;
