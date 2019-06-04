aa313e6bc06a61c8422befc748652da938ae1a28
ShaunKolich <employmeinc@gmail.com>
2019-06-04 11:44:30 -0400

    Finished the Fluid Grid Layout for Site- Starting the fillin of information

--- src/components/js/websiteDisplay.js
+++ src/components/js/websiteDisplay.js
@@ -8,58 +8,23 @@ function DisplayWebsite() {
         <div className="main_Container">
             <Container>
                 <Row>
-                    <Col xs="8">Logo Here</Col>
+                    <div className ="logo_container">
+                    <Col xs="8"></Col>
+                    <div className="logo">Logo Here
+                    </div>
+                    </div>
                     <Col xs="4">Contact</Col>
-                    <Col xs="8">Logo Here 2</Col>
+                    <div className="contactInfo">
+                    </div>
+                    <Col xs="8"></Col>
                     <Col xs="4">Main Navigation</Col>
+                    <div className="topNav">
+                    </div>
                 </Row>
 
-                <Row>
-                    <Col xs="12">
-                        Header Image
-                <Row>
-                            <Col xs="10" sm="6">
-                                Header Image To
-                    </Col>
-                            <Col xs="2" sm="6">
-                                Request a Quote
-                    </Col>
-                        </Row>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs="12">
-                        Our Services
-                <Row>
-                            <Col xs="8" sm="6">
-                                Boxes of Services
-                    </Col>
-                            <Col xs="4" sm="6">
-                                Contact Form
-                    </Col>
-                        </Row>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>About Us</Col>
-                    <Col>Picture</Col>
-                </Row>
-                <Row>
-                    <Col xs="9">
-                        Our Products
-                <Row>
-                            <Col xs="8" sm="6">
-                                Products
-                    </Col>
-                        </Row>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>Testimonials</Col>
-
-                </Row>
             </Container>
         </div>
+
     )
 }
 
